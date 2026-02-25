f26132aeee9170f8970d882cfcf0d7f8c72f5e20
WenchaoZhu0128 <wz2754@columbia.edu>
2026-02-25 18:29:15 -0500

    Update bond price calculation method
    Refactor cash flow calculation for bond price.

--- BondPrice_File.py
+++ BondPrice_File.py
@@ -3,7 +3,8 @@ import numpy as np
 def getBondPrice(y, face, couponRate, m, ppy=1):
     t = np.arange(1, (ppy*m)+1)
     pv = (1 + y/ppy)**(-t)
-    pvcf = couponRate * face / ppy
-    pvcfsum = pvcf * pv.sum()
+    cf = couponRate * face / ppy
+    pvcf = pv * cf
+    pvcfsum = pvcf.sum()
     bondprice = pvcfsum + face * pv[-1]
     return(bondprice)
